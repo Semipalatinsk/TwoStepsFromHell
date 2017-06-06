6c68b50e22120fbdf0af579fd3ebfb5d69200c5a
dggg123567 <dggg123567@gmail.com>
2017-06-06 21:42:39 +0800

    Import class in ViewController

--- TwoStepsFromHell/TrackViewController.swift
+++ TwoStepsFromHell/TrackViewController.swift
@@ -8,7 +8,11 @@
 
 import UIKit
 
-class MusiclistViewController: UIViewController {
+
+
+class TrackViewController: UIViewController {
+    
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,16 +24,5 @@ class MusiclistViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
